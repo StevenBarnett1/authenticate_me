81eb06c6eae4d1fd64d5ead4a0d7507e1b344c8c
Stevenbarnett1 <Stevenbarnett1@live.com>
2021-09-14 13:54:55 -0700

    having issue with splash image

--- frontend/src/components/Splash/index.js
+++ frontend/src/components/Splash/index.js
@@ -4,7 +4,9 @@ import "./Splash.css"
 const Splash = () => {
 
     return (
+        <div>
         <img id = "splash" src = "https://a0.muscache.com/im/pictures/57b9f708-bb12-498c-bc33-769f8fc43e63.jpg?im_w=2560"></img>
+        </div>
     )
 }
 
