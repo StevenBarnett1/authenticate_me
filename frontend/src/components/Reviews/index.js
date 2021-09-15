8765b95cfef54aca26cc49ae887cf4dfb5351679
Stevenbarnett1 <Stevenbarnett1@live.com>
2021-09-15 16:05:10 -0700

    got reviews working on spot page

--- frontend/src/components/Reviews/index.js
+++ frontend/src/components/Reviews/index.js
@@ -1,17 +1,28 @@
 import { useDispatch, useSelector } from "react-redux"
 import {getReviews} from "../../store/reviews"
 import {useEffect} from "react"
+import "./Reviews.css"
+const monthNames = ["January", "February", "March", "April", "May", "June",
+  "July", "August", "September", "October", "November", "December"
+];
+
 const Reviews = ({spot}) => {
     let dispatch = useDispatch()
 
     useEffect(() => {
         if (spot)dispatch(getReviews(spot.id))
-    }, [dispatch])
+    }, [dispatch,spot])
 
     let reviews = useSelector((state)=>state.reviews)
     reviews = Object.values(reviews)
-    console.log("REVIEWSSSSSS",reviews)
+    reviews = reviews.map(review => {
+        let date = new Date(review.createdAt)
+        let month = monthNames[date.getMonth()]
+        let day = date.getDate()
 
+        return {...review,createdAt:`${month} ${day}`}
+    })
+    console.log("REVIEWSSSSSS",reviews)
     return (
         <>
             <div id = "spot-overall-review"></div>
@@ -19,8 +30,13 @@ const Reviews = ({spot}) => {
                 {reviews && reviews.map(review => (
                     <div key = {review.id} id = "individual-review-container">
                         <div id = "individual-review-container-top">
-                            <div>{review.User.firstName}</div>
-                            <div>{typeof review.createdAt}</div>
+                            <div id = "individual-review-container-top-left">
+                                <img src = {review.User.image}></img>
+                            </div>
+                            <div id = "individual-review-container-top-right">
+                                <div>{review.User.firstName}</div>
+                                <p>{review.createdAt}</p>
+                            </div>
                         </div>
                         <div>{review.body}</div>
                     </div>
