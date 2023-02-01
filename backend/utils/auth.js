3bb21cbb4bc8726f83d006ce85ae204b138ace16
stevenbarnett1 <stevenbarnett1@live.com>
2023-02-01 11:33:02 -0800

    comment out expires in jwt/cookie stuff

--- backend/utils/auth.js
+++ backend/utils/auth.js
@@ -10,15 +10,15 @@ const setTokenCookie = (res, user) => {
     // Create the token.
     const token = jwt.sign(
       { data: user.toSafeObject() },
-      secret,
-      { expiresIn: parseInt(expiresIn) }, // 604,800 seconds = 1 week
+      secret
+      //{ expiresIn: parseInt(expiresIn) }, // 604,800 seconds = 1 week
     );
 
     const isProduction = process.env.NODE_ENV === "production";
 
     // Set the token cookie
     res.cookie('token', token, {
-      maxAge: expiresIn * 1000, // maxAge in milliseconds
+      //maxAge: expiresIn * 1000, // maxAge in milliseconds
       httpOnly: true,
       secure: isProduction,
       sameSite: isProduction && "Lax",
